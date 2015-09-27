c664dcb026ad8338ba04a039bd9be824e7075c22
Felix Ahlcrona <felix_pro2@hotmail.com>
2015-09-27 23:13:48 +0200

    Hardware-information v.1
    > Added most of the functions
    
    > Need to fix a nice WPF design.

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Management;
-
+using System.Diagnostics;
 
 
 namespace WpfApplication1
@@ -50,7 +50,7 @@ namespace WpfApplication1
             ManagementClass mgt = new ManagementClass("Win32_Processor");
             ManagementObjectCollection procs = mgt.GetInstances();
             foreach (ManagementObject item in procs)
-                return item.Properties["MaxClockSpeed"].Value.ToString();
+                return item.Properties["MaxClockSpeed"].Value.ToString()+ " Mhz";
 
 
             return "Unknown";
@@ -78,7 +78,7 @@ namespace WpfApplication1
             ManagementClass mgt = new ManagementClass("Win32_Processor");
             ManagementObjectCollection procs = mgt.GetInstances();
             foreach (ManagementObject item in procs)
-                return item.Properties["NumberOfLogicalProcessors"].Value.ToString();
+                return item.Properties["NumberOfCores"].Value.ToString();
 
 
             return "Unknown";
@@ -94,9 +94,111 @@ namespace WpfApplication1
 
             return "Unknown";
         }
-     
+
+        private static string GetGPUDriver()
+        {
+            ManagementClass mgt = new ManagementClass("Win32_VideoController");
+            ManagementObjectCollection procs = mgt.GetInstances();
+            foreach (ManagementObject item in procs)
+                return item.Properties["DriverVersion"].Value.ToString();
+            
+            return "Unknown";
+        }
+
+
+
+        private static string GetGPURefreshRate()
+        {
+            ManagementClass mgt = new ManagementClass("Win32_VideoController");
+            ManagementObjectCollection procs = mgt.GetInstances();
+            foreach (ManagementObject item in procs)
+                return item.Properties["MaxRefreshRate"].Value.ToString() + " HZ";
            
-    
+            return "Unknown";
+        }
+
+
+     
+
+        private static string GetHardDriveName()
+        {
+            ManagementClass mgt = new ManagementClass("Win32_BaseBoard");
+            ManagementObjectCollection procs = mgt.GetInstances();
+            foreach (ManagementObject item in procs)
+                return item.Properties["Product"].Value.ToString();
+
+            return "Unknown";
+        }
+
+        private static string GetHardDriveHotSwap()
+        {
+            ManagementClass mgt = new ManagementClass("Win32_BaseBoard");
+            ManagementObjectCollection procs = mgt.GetInstances();
+            foreach (ManagementObject item in procs)
+                return item.Properties["HotSwappable"].Value.ToString();
+
+            return "Unknown";
+        }
+
+
+        private static string GetRamSize()
+        {
+            ManagementClass mgt = new ManagementClass("Win32_PhysicalMemory");
+            ManagementObjectCollection procs = mgt.GetInstances();
+            foreach (ManagementObject obj in procs)
+            {
+                long MemorySize = 0;
+                long mCap = 0;
+                mCap = Convert.ToInt64(obj["Capacity"]);
+                MemorySize += mCap;
+                MemorySize = (MemorySize / 1024) / 1024;
+                return MemorySize.ToString() + "MB";
+
+
+            }
+
+            return "Unknown";
+        }
+
+
+
+
+        public static string GetRamSlots()
+        {
+
+            int MemSlots = 0;
+            ManagementScope oMs = new ManagementScope();
+            ObjectQuery oQuery2 = new ObjectQuery("SELECT MemoryDevices FROM Win32_PhysicalMemoryArray");
+            ManagementObjectSearcher oSearcher2 = new ManagementObjectSearcher(oMs, oQuery2);
+            ManagementObjectCollection oCollection2 = oSearcher2.Get();
+            foreach (ManagementObject obj in oCollection2)
+            {
+                MemSlots = Convert.ToInt32(obj["MemoryDevices"]);
+
+            }
+            return MemSlots.ToString();
+        }
+
+
+
+        public static string GetCPUBit()
+        {
+            ManagementClass mgt = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection procs = mgt.GetInstances();
+            foreach (ManagementObject obj in procs)
+            {
+                long CPUBit = 0;
+                long mCap = 0;
+                mCap = Convert.ToInt64(obj["AddressWidth"]);
+                CPUBit += mCap;
+
+                return CPUBit.ToString() + " bit";
+
+
+            }
+
+            return "Unknown";
+        }
 
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -145,6 +247,57 @@ namespace WpfApplication1
             this.Title = textBox.Text = (GetCPUTotalCores());
         }
 
+        private void TextBox_TextChanged_5(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetGPUDriver());
+        }
+
+
+        public void TextBox_TextChanged_6(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetGPURefreshRate());
+        }
+
+        private void TextBox_TextChanged_7(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetHardDriveHotSwap());
+        }
+
+        private void TextBox_TextChanged_8(object sender, TextChangedEventArgs e)
+        {
+
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetHardDriveName());
+        }
+
+        private void TextBox_TextChanged_9(object sender, TextChangedEventArgs e)
+        {
+
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetRamSize());
+        }
+
+        private void TextBox_TextChanged_10(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetRamSlots());
+        }
+
+        private void TextBox_TextChanged_11(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            this.Title = textBox.Text = (GetCPUBit());
+        }
 
      
 
